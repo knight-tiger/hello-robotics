9293975d776ed6eb88f1c73c467ac25cae9030e1
Tanmay Naik <132657650+knight-tiger@users.noreply.github.com>
2025-10-02 23:52:38 +0530

    Create vowel_or_not.c

---
+++ vowel_or_not.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{ 
+    //vowel or consonant
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+        printf("%c is a vowel.", ch);
+    else
+        printf("%c is a consonant.", ch);
+}
