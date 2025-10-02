10538a3ed547de1f0504caa964597b6a604c7c7e
Tanmay Naik <132657650+knight-tiger@users.noreply.github.com>
2025-10-02 23:12:52 +0530

    Create number_printer.c

---
+++ number_printer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter how many numbers you want to print: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d\n",i);
+    }
+}
