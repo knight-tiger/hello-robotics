8ee970574d8efeb6c08f61d2d14a30a5c8728036
Tanmay Naik <132657650+knight-tiger@users.noreply.github.com>
2025-10-04 00:24:27 +0530

    Create continue_number_pattern.c

---
+++ continue_number_pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{ //printing a square
+    int side;
+    printf("Enter the side length of the square\n");
+    scanf("%d", &side);
+    char ch='A';
+
+    for(int i = 0; i < side; i++)
+    {
+        for(int j = 0; j<=i; j++)
+        { 
+            printf("%c ",ch);
+        }
+        ch++;
+        printf("\n");
+    }
+
+    return 0;
+
+
+}
