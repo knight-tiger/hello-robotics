8dab61a1bcb5ceb9f27666397669c7bbcd9e7623
Tanmay Naik <132657650+knight-tiger@users.noreply.github.com>
2025-10-04 00:05:22 +0530

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{ //printing a inverted triangle with Alphabets
+    
+    int side;
+    printf("Enter the side length of the triangle\n");
+    scanf("%d", &side);
+
+    for(int i = 0; i < side; i++)
+    {
+        for(int j = 0; j < side - i; j++)
+        {
+    
+            printf("%c ", j + 'A');
+            printf("  ");
+          
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+
+}
