2bcfc61a526313e7c1231e508853a04048b9b910
Tanmay Naik <132657650+knight-tiger@users.noreply.github.com>
2025-10-04 00:39:52 +0530

    Create hollow_alphabet_pyramid.c

---
+++ hollow_alphabet_pyramid.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{ //printing a hollow number pyramid
+    int i,j,k,n;
+    printf("Enter the number of rows:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+            printf(" ");
+        for(k=1;k<=(2*i-1);k++)
+        {
+            if(k==1 || k==(2*i-1) || i==n)
+            {
+                printf("%c",k + 'A'-1);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+
+
+}
