4b378ee9e189f4c7e501196ac15a2b21382e27d0
Tanmay Naik <132657650+knight-tiger@users.noreply.github.com>
2025-11-10 00:00:16 +0530

    code to store student details

---
+++ struct1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+//code to store student info in struct
+int main()
+{
+    struct student
+    {
+        char name[20];
+        int roll;
+        char div;
+    
+    }s1;  
+    printf("...THIS PROGRAM WILL STORE STUDENT DATA...");
+
+    printf("\nEnter your name: ");
+    scanf("%s",&s1.name);
+
+    printf("\nEnter your roll no: ");
+    scanf("%d",&s1.roll);
+    
+    printf("\nEnter your div: ");
+    scanf(" %c",&s1.div);
+
+    printf("Name: %s",s1.name);
+     printf("\nRoll no: %d",s1.roll);
+      printf("\nDiv: %c",s1.div);
+
+   
+
+    return 0;
+}
