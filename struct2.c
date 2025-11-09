ee0d459c032ff77d45eafb5aeb821f4a068bafa0
Tanmay Naik <132657650+knight-tiger@users.noreply.github.com>
2025-11-10 00:03:53 +0530

    Add program to store student information using typedef struct

---
+++ struct2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+//code to store student info in struct using typedef
+int main()
+{
+    typedef struct student
+    {
+        char name[20];
+        int roll;
+        char div;
+    
+    }stu;  
+    stu s1;  
+    printf("...THIS PROGRAM WILL STORE STUDENT DATA...");
+
+    printf("\nEnter your name: ");
+    scanf("%s",&s1.name);
+
+    printf("\nEnter your roll no: ");
+    scanf("%d",&s1.roll);
+    
+    printf("\nEnter your div: ");
+    scanf(" %c",&s1.div);
+
+    printf("Name: %s",s1.name);
+     printf("\nRoll no: %d",s1.roll);
+      printf("\nDiv: %c",s1.div);
+
+   
+
+    return 0;
+}
